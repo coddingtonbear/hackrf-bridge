2ca90ebcc22a197f05eb40ed1a40091194c7493d
Adam Coddington <me@adamcoddington.net>
2015-11-04 18:49:10 -0800

    X

--- hackrf_rtlsdr_soundcard_adapter.py
+++ hackrf_rtlsdr_soundcard_adapter.py
@@ -2,7 +2,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Wed Nov  4 18:46:44 2015
+# Generated: Wed Nov  4 18:49:07 2015
 ##################################################
 
 from gnuradio import analog
@@ -47,9 +47,9 @@ class top_block(gr.top_block):
         self.osmosdr_sink_0.set_sample_rate(hackrf_rate)
         self.osmosdr_sink_0.set_center_freq(out_frequency-out_frequency_offset, 0)
         self.osmosdr_sink_0.set_freq_corr(4, 0)
-        self.osmosdr_sink_0.set_gain(0, 0)
-        self.osmosdr_sink_0.set_if_gain(10, 0)
-        self.osmosdr_sink_0.set_bb_gain(10, 0)
+        self.osmosdr_sink_0.set_gain(14, 0)
+        self.osmosdr_sink_0.set_if_gain(30, 0)
+        self.osmosdr_sink_0.set_bb_gain(30, 0)
         self.osmosdr_sink_0.set_antenna("0", 0)
         self.osmosdr_sink_0.set_bandwidth(100e3, 0)
           
