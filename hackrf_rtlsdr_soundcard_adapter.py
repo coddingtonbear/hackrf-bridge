70515b3746a713d189bdddc7ba2a3135ccaadedd
Adam Coddington <me@adamcoddington.net>
2015-11-04 19:33:15 -0800

    X

--- hackrf_rtlsdr_soundcard_adapter.py
+++ hackrf_rtlsdr_soundcard_adapter.py
@@ -2,7 +2,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Wed Nov  4 19:31:46 2015
+# Generated: Wed Nov  4 19:33:11 2015
 ##################################################
 
 from gnuradio import analog
@@ -64,7 +64,7 @@ class top_block(gr.top_block):
         self.analog_fm_demod_cf_0 = analog.fm_demod_cf(
         	channel_rate=audio_rate*5,
         	audio_decim=5,
-        	deviation=dstar_bandwidth*2,
+        	deviation=dstar_bandwidth,
         	audio_pass=dstar_bandwidth,
         	audio_stop=dstar_bandwidth+1e3,
         	gain=1,
