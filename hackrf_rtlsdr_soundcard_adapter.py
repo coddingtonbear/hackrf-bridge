f2dc1ef30e6664ceffe051434cfef726265a05c7
Adam Coddington <me@adamcoddington.net>
2015-11-05 11:37:22 -0800

    X

--- hackrf_rtlsdr_soundcard_adapter.py
+++ hackrf_rtlsdr_soundcard_adapter.py
@@ -2,7 +2,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Nov  5 11:36:37 2015
+# Generated: Thu Nov  5 11:37:20 2015
 ##################################################
 
 from gnuradio import analog
@@ -29,7 +29,7 @@ class top_block(gr.top_block):
         self.in_frequency_offset = in_frequency_offset = 0
         self.in_frequency = in_frequency = 145.551e6
         self.in_final_gain = in_final_gain = 4
-        self.in_audio_inverted = in_audio_inverted = False
+        self.in_audio_inverted = in_audio_inverted = True
         self.dstar_bandwidth = dstar_bandwidth = 6.5e3
         self.audio_rate = audio_rate = int(48e3)
 
