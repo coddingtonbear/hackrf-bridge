cc850f90eb6b7cab5ba0c9e8fab308e9f1192405
Adam Coddington <me@adamcoddington.net>
2015-11-05 08:12:16 -0800

    X

--- test2.py
+++ test2.py
@@ -23,13 +23,13 @@ class top_block(gr.top_block):
         # Blocks
         ##################################################
         self.blocks_null_source_0 = blocks.null_source(gr.sizeof_float*1)
-        time.sleep(5)
         self.audio_sink_0 = audio.sink(int(48e3), "hw:0,0", True)
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.blocks_null_source_0, 0), (self.audio_sink_0, 0))    
+        time.sleep(5)
 
 
 
