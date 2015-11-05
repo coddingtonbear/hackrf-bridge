47266dfa0db3bf22429b14aa6285198b72fd194f
Adam Coddington <me@adamcoddington.net>
2015-11-05 13:48:03 -0800

    X

--- hackrf_rtlsdr_soundcard_adapter.py
+++ hackrf_rtlsdr_soundcard_adapter.py
@@ -2,7 +2,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Nov  5 13:46:52 2015
+# Generated: Thu Nov  5 13:47:59 2015
 ##################################################
 
 from gnuradio import analog
@@ -65,8 +65,8 @@ class top_block(gr.top_block):
         	channel_rate=audio_rate*3,
         	audio_decim=3,
         	deviation=dstar_bandwidth*2,
-        	audio_pass=dstar_bandwidth+1e3,
-        	audio_stop=dstar_bandwidth+2e3,
+        	audio_pass=dstar_bandwidth*2,
+        	audio_stop=dstar_bandwidth*3,
         	gain=1,
         	tau=0,
         )
